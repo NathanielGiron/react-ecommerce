46225aa056ae4c8cb0a5bbbb7b08eeead06e3a4f
Nathaniel Giron <nathaniel.giron@gmail.com>
2020-02-23 23:58:03 -0800

    designed the homepage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import HomePage from './homepage.component'
 
 function App() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <HomePage />
     </div>
   );
 }
