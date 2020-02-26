b3b23894fb8b872c500ee1842d954394a4664dac
Nathaniel Giron <nathaniel.giron@gmail.com>
2020-02-25 23:21:24 -0800

    created shop page and collection preview component

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -6,7 +6,6 @@ const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
   <div 
     className={`${size} menu-item`}
     onClick={() => history.push(`${match.url}${linkUrl}`)}>
-      {console.log(history)}
     <div 
       className='background-image'
       style={{
