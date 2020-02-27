72aabc3d9e43a93a3942aadc1bac849d5a6b9d5d
Nathaniel Giron <nathaniel.giron@gmail.com>
2020-02-27 00:06:19 -0800

    added currentUser state to know when the user is authenticated

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -4,7 +4,6 @@ import './custom-button.styles.scss';
 const CustomButton = ({ children, ...otherProps }) => (
   <button className='custom-button' {...otherProps}>
     {children}
-    {console.log(otherProps)}
   </button>
 );
 
